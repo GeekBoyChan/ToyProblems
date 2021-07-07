07f63fcfe6f0ed2277f49b36343cfec6454e58b4
Chandler Ocapan <Cocapan@gmail.com>
2021-07-07 11:29:56 -0700

    linked list practice, implemented list data function

--- linkedList.js
+++ linkedList.js
@@ -15,4 +15,16 @@ class LinkedList {
   insertFirst(data) {
     this.head = new Node(data, this.head);
   }
+
+  //Print List Data
+  listData() {
+    if (this.head === null || this.size === 0) {
+      return;
+    }
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
 }
