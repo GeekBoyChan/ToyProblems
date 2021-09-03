8c5f26a4cd9906a16cdb64765a59a13505c36a7b
Chandler Ocapan <Cocapan@gmail.com>
2021-09-03 11:24:19 -0700

    queueTwoStack toy problem, stack/ queue practice

---
+++ icToyProblems/StacksAndQueus/QueueTwoStacks.js
@@ -0,0 +1,110 @@
+class Stack {
+  constructor() {
+    // Initialize an empty stack
+    this.items = [];
+  }
+
+  // Push a new item onto the stack
+  push(item) {
+    this.items.push(item);
+  }
+
+  // Remove and return the last item
+  pop() {
+    // If the stack is empty, return null
+    // (It would also be reasonable to throw an exception)
+    if (!this.items.length) {
+      return null;
+    }
+    return this.items.pop();
+  }
+
+  // Return the last item without removing it
+  peek() {
+    if (!this.items.length) {
+      return null;
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  length() {
+    return this.items.length;
+  }
+}
+
+//  Implement the enqueue and dequeue methods
+
+class QueueTwoStacks {
+  constructor() {
+    this.inStack = new Stack();
+    this.outStack = new Stack();
+  }
+
+  enqueue(item) {
+    this.inStack.push(item);
+  }
+
+  dequeue() {
+    if (this.inStack.length() === 0 && this.outStack.length() === 0) {
+      throw new Error('Cannot dequeue an empty queue');
+    }
+
+    if (this.outStack.length() === 0) {
+      while (this.inStack.length() > 0) {
+        let inStackPop = this.inStack.pop();
+        this.outStack.push(inStackPop);
+      }
+    }
+    return this.outStack.pop();
+  }
+}
+
+// Tests
+const q = new QueueTwoStacks();
+
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+
+let desc = 'dequeue #1';
+let actual = q.dequeue();
+let expected = 1;
+assertEquals(actual, expected, desc);
+
+desc = 'dequeue #2';
+actual = q.dequeue();
+expected = 2;
+assertEquals(actual, expected, desc);
+
+q.enqueue(4);
+
+desc = 'dequeue #3';
+actual = q.dequeue();
+expected = 3;
+assertEquals(actual, expected, desc);
+
+desc = 'dequeue #4';
+actual = q.dequeue();
+expected = 4;
+assertEquals(actual, expected, desc);
+
+desc = 'dequeue from empty queue';
+const emptyDequeue = () => q.dequeue();
+assertThrowsError(emptyDequeue, desc);
+
+function assertEquals(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
+
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
+}
